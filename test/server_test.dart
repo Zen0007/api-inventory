7e5a4dc3ddb5925b47f1fb318282ac42f851aa46
Zen0007 <zmr83888@gmail.com>
2025-09-13 00:31:37 +0700

    merge

--- test/server_test.dart
+++ test/server_test.dart
@@ -1,15 +1,6 @@
 import 'dart:convert';
-import 'dart:io';
 import 'package:test/test.dart';
 import 'package:web_socket_channel/io.dart';
-
-Future<int> _findAvailablePort() async {
-  final server = await ServerSocket.bind(InternetAddress.anyIPv4, 0);
-  print(server.port);
-  final port = server.port;
-  await server.close();
-  return port;
-}
 
 void main() {
   late IOWebSocketChannel channels;
